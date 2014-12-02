887f21242b3c3c02bb9bc02c86e0442f5e9c8358
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-12-02 08:58:36 -0800

    modify to use Ring, Compojure, H2, Hiccup

--- project.clj
+++ project.clj
@@ -1,12 +1,15 @@
 (defproject allocations "0.1.0-SNAPSHOT"
+  :dependencies [[com.h2database/h2 "1.3.170"]
+                 [compojure "1.2.0"]
+                 [hiccup "1.0.2"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [ring/ring-defaults "0.1.2"]]
   :description "C5 Resource Allocations"
-  :url "http://www.carbonfive.com"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [http-kit "2.1.16"]
-                 [ring/ring-devel "1.1.8"]
-                 [ring/ring-core "1.1.8"]]
-
-  :main ^:skip-aot allocations.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :min-lein-version "2.0.0"
+  :plugins [[lein-ring "0.8.13"]]
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"] [ring-mock "0.1.5"]]}}
+  :ring {:handler allocations.handler/app}
+  :url "http://www.carbonfive.com"
+)
