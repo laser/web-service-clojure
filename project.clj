02e08e965e86b5e330168c894ceb00e422c63f0c
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-12-01 17:04:48 -0800

    WIP: fix hot-swapping

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://www.carbonfive.com"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [http-kit "2.1.16"]]
+                 [http-kit "2.1.16"]
+                 [ring/ring-devel "1.1.8"]
+                 [ring/ring-core "1.1.8"]]
+
   :main ^:skip-aot allocations.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/allocations/core.clj
+++ src/allocations/core.clj
@@ -1,7 +1,12 @@
 (ns allocations.core
+  (:use [org.httpkit.server :only [run-server]])
+  (:require [ring.middleware.reload :as reload])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello folks"})
+
+(defn -main [& args]
+  (run-server (reload/wrap-reload handler) {:port 8080}))
