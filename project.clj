43f6d9574dbc35a3515efc76c41b2d4bd07d737f
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-12-02 09:04:56 -0800

    prepare for deployment

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
                  [hiccup "1.0.2"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [ring/ring-jetty-adapter "1.3.2"]]
   :description "C5 Resource Allocations"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
+  :main allocations.handler
   :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.8.13"]]
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"] [ring-mock "0.1.5"]]}}
