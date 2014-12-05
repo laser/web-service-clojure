f462ebd7010d53f0184cd429c3a4a011faaf4d69
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-12-05 15:13:34 -0800

    add Ring request-level tests

--- test/tutorial/handler_test.clj
+++ test/tutorial/handler_test.clj
@@ -1,7 +1,7 @@
 (ns tutorial.handler_test
   (:require [clojure.test :refer :all]
             [tutorial.handler :refer :all]
-            [ring.mock.request :refer [request]]
+            [ring.mock.request :refer [request content-type]]
             [clojure.data.json :as json]
             [ring.util.io :as io]
             [environ.core :refer [env]]
@@ -17,20 +17,58 @@
                     :migrator "db/migrations"})
   (f))
 
-(use-fixtures :once with-test-db)
+(use-fixtures :each with-test-db)
 
 (defn responsify
   ([] (responsify 200))
   ([code] (responsify code nil))
   ([code body] (responsify code body {}))
-  ([code body headers] {:status code
-                        :body body
-                        :headers (merge {"Content-Type" "application/json; charset=utf-8"} headers)}))
+  ([code body headers] (let [defaults {:status code :body body :headers {}}]
+                         (if (nil? body)
+                           defaults
+                           (->> {"Content-Type" "application/json; charset=utf-8"}
+                                (merge headers)
+                                (hash-map :headers)
+                                (merge defaults))))))
 
-(deftest handler-not-found
-  (is (= (responsify 404 nil {"Content-Type" "application/json"})
-         (app (request :get "/doc/10")))))
+(defn api-req
+  [& args]
+  (app (content-type (apply request args) "application/json")))
 
-(deftest handler-get-todos-empty
-  (is (= (responsify 200 (json/write-str []))
-         (app (request :get "/todos")))))
+(def api-post #(api-req :post %1 (json/write-str %2)))
+(def api-patch #(api-req :patch %1 (json/write-str %2)))
+(def api-get (partial api-req :get))
+(def api-delete (partial api-req :delete))
+
+(defn parse-resp
+  [resp]
+  (update-in resp [:body] #(if % (json/read-str %) nil)))
+
+(deftest handler-lifecycle
+  ;;; missing route
+  (is (= (responsify 404)
+         (parse-resp (api-get "/doc/10"))))
+
+  ;;; create a todo
+  (let [user-data {"text" "foo" "completed" false }]
+    (is (= (responsify 201 (assoc user-data "id" 1) {"Location" "http://localhost:80/todos/1"})
+           (parse-resp (api-post "/todos" user-data)))))
+
+  ;;; update a todo
+  (let [user-data {"text" "bar" "completed" true "id" 1}]
+    (is (= (responsify 200 user-data)
+           (parse-resp (api-patch "/todos/1" user-data)))))
+
+  ;;; read all todos
+  (let [user-data {"text" "bar" "completed" true "id" 1}]
+    (is (= (responsify 200 [user-data])
+           (parse-resp (api-get "/todos")))))
+
+  ;;; read a single todo
+  (let [user-data {"text" "bar" "completed" true "id" 1}]
+    (is (= (responsify 200 user-data)
+           (parse-resp (api-get "/todos/1")))))
+
+  ;;; delete a todo
+  (is (= (responsify 204)
+         (parse-resp (api-delete "/todos/1")))))
