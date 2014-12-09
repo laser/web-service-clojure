b60d50f0838ee63f1dcf4a154e66485cec233fce
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-12-09 13:15:12 -0800

    clean up handler using map deconstruction

--- src/tutorial/handler.clj
+++ src/tutorial/handler.clj
@@ -2,36 +2,25 @@
   (:require [tutorial.data :as data]
             [tutorial.http :as http]))
 
+
+(defn patch-todo
+  [{text :text completed :completed :as body} {id :id}]
+  (let [id (read-string id)]
+    (http/ok (do (data/update-todo-by-id! id text completed)
+                 (assoc body :id id)))))
+
 (defn post-todo
-  [{{text :text completed :completed} :body :as req}]
-  (let [results (->> (data/create-todo<! (get-in req [:body :text]) (get-in req [:body :completed]))
+  [{{text :text completed :completed :as body} :body :as req}]
+  (let [results (->> (data/create-todo<! text completed)
                      vals
                      first
-                     (assoc {:text text, :completed completed} :id))]
+                     (assoc body :id))]
     (->> results
          :id
          (http/url-for req)
          (http/created results))))
 
-(defn get-todos
-  []
-  (http/ok (data/read-todos)))
-
-(defn get-todo
-  [id]
-  (http/ok (first (data/read-todos-by-id id))))
-
-(defn patch-todo
-  [{text :text completed :completed} {id :id}]
-  (let [id (read-string id)]
-    (http/ok (do (data/update-todo-by-id! id text completed)
-                 {:id id, :text text, :completed completed}))))
-
-(defn delete-todo
-  [id]
-  (data/delete-todo-by-id! id)
-  (http/no-content))
-
-(defn not-found
-  []
-  (http/not-found))
+(def delete-todo #(do (data/delete-todo-by-id! %) (http/no-content)))
+(def get-todos #(http/ok (data/read-todos)))
+(def get-todo #(http/ok (first (data/read-todos-by-id %))))
+(def not-found http/not-found)
